7a8aa433f16cefa2d1546a4dafde87bc5706803a
Justin Yip <justinyip@Justins-MacBook-Air.local>
2018-02-26 10:49:13 -0800

    third push

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ console.log('alex says...');
 console.log('alex added from personal fork');
 for (var i = 0; i < 2; i++) {
   if (i === 0) {
-
+sdfadsfafsadfs
     console.log('alex is number 1');
   }
 }
