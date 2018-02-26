87fdea98d62956da708d1ea64a3f718a95f94029
scottyb <scottyb@scottybs-MacBook.local>
2018-02-26 11:02:34 -0800

    scott changes

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
-console.log('alex says that React Velocity is a great project name...');
 console.log('alex says...');
-console.log('alex added from personal fork');
-for (var i = 0; i < 2; i++) {
+ fork');
+for (var i = 0; i < 3; i++) {
   if (i === 0) {
     console.log('alex is number 1');
   }
