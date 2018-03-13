94c009d37866041df492a41193b5add41fb56dcf
Justin Yip <justinyip@Justins-MacBook-Air.local>
2018-03-13 14:20:23 -0700

    deleted node modules

--- node_modules/react-display-name/lib/getDisplayName.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = getDisplayName;
-function getDisplayName(Component) {
-  return Component.displayName || Component.name || (typeof Component === 'string' && Component.length > 0 ? Component : 'Unknown');
-}
