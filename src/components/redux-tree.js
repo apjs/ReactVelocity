0a7956cf47343ac8f7ce4ee82b67f72256e21e56
Justin Yip <justinyip@Justins-MacBook-Air.local>
2018-03-16 14:32:35 -0700

    cant make redux components children

--- src/components/redux-tree.js
+++ src/components/redux-tree.js
@@ -17,7 +17,11 @@ class ReduxTree extends Component {
     super(props);
 
     this.state = {
-      treeData: [{ name: 'Actions'},{ name: 'Reducers'},{ name: 'Containers'},{ name: 'Components'}],
+      treeData: [
+        { name: 'Actions', parent: true},
+        { name: 'Reducers', parent: true},
+        { name: 'Containers', parent: true},
+        { name: 'Components', parent: true}],
       flattenedData: ['App','Reducers','Containers','Components'],
       textFieldValue: '',
       flattenedArray: [],
@@ -149,6 +153,12 @@ class ReduxTree extends Component {
 
   render() {
     const getNodeKey = ({ treeIndex }) => treeIndex;
+    const canDrop = ({ node, nextParent, prevPath, nextPath }) => {
+      if (node.parent) {
+        return false;
+      }
+      return true;
+    };
     
     return (
       <div>
@@ -169,6 +179,7 @@ class ReduxTree extends Component {
           <SortableTree
             treeData={this.state.treeData}
             onChange={treeData => this.setState({ treeData })}
+            canDrop={canDrop}
             generateNodeProps={({ node, path }) => ({
               title: (
                 <input
