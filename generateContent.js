9ce83506b4935a1e35ef6571fcb43d6716dcece5
Paul Dubovsky <pauldubovsky@Pauls-MacBook-Air.local>
2018-03-16 14:24:48 -0700

    .

--- generateContent.js
+++ generateContent.js
@@ -1,25 +1,3 @@
-// export function generateCode(data) {
-//   let code = '';
-//   for (let i = 0; i < data.length; i++) {
-//     code += "import React, { Component } from 'react';\n\n"
-//     code += `class extends ${data[i].title} {\n`;
-//     code += '  render() {\n';
-//     code += '    return (\n';
-//     code += '      <div>\n';
-//     if (data[i].children) {
-//       for (let j=0; j < data[i].children.length; j++) {
-//         code += `        <${data[i].children[j].title} />\n`;
-//       }
-//     }
-//     code += '      </div>\n';
-//     code += '    );\n';
-//     code += '  };\n';
-//     code += '}\n\n';
-//     code += `export default ${data[0].title};`;
-//   }
-//   return code;
-// }
-
 export function generateCode(data) {
   let filesToZip = {};
   let keys = Object.keys(data);
@@ -52,6 +30,3 @@ export function generateCode(data) {
   }
   return filesToZip;
 }
-
-// export const treeData = [{title: 'App', children: [{title: 'Bengt', children: [{title: 'Einar'}]}, {title: 'Daniel'}]}];
-// export const version2 = {"App":["Scott"],"Scott":["Justin"],"Justin":null,"Alex":null}
