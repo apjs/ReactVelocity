ced10e6a86887a3a768db494788b70cf2b3bd4d5
alex clifton <oooalexoooo@gmail.com>
2018-03-22 08:59:54 -0700

    ben helped us set up google analytics

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ReactTree from './react-tree';
 import ReduxTree from './redux-tree';
-
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-116211451-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 class App extends Component {
     render () {
       return (
